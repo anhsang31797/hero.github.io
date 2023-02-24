8ea60b63885ce1d7b60baa7d9c9f3c6e7311d2a2
anhsang31797 <anhsang31797@gmail.com>
2023-02-24 09:19:09 +0700

    3019

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  public loginUser = '';
+
+  constructor (){}
+
+  public ngOnInit () {
+
+  }
 
 }
+
+
+
