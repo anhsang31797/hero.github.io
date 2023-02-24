c4b392067954b34d1c3a279ff77cf8fb40603257
anhsang31797 <anhsang31797@gmail.com>
2023-02-24 09:05:29 +0700

    3017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { OnSalePipe } from './on-sale.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     HomeComponent,
-    OnSalePipe
+    OnSalePipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
