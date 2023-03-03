d4a1ba7993c0f709ecaaadcdf13f976f7a9a6940
anhsang31797 <anhsang31797@gmail.com>
2023-03-03 13:37:49 +0700

    rsjx - ep 5

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,44 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+  styleUrls: ['./rxjs.component.css'],
 })
 export class RXJSComponent implements OnInit {
-
   public agents: Observable<string> | undefined;
-  public agentName : string | null | undefined;
+  public agentName: string | null | undefined;
 
-  constructor(){}
+  public arrayStudents = ['Sang', 'Hoa', 'Uyên'];
+  public listStudents = 'Sang';
+  public objectStudents = [
+    {
+      id: 1,
+      name: 'Sang',
+    },
+    {
+      id: 2,
+      name: 'Hoa',
+    },
+    {
+      id: 3,
+      name: 'Uyên',
+    },
+  ];
 
-  ngOnInit():void{
+  //public students: Observable<string[]> | undefined = of(this.arrayStudents);
 
-    this.agents = new Observable(
-      function (observer){
-        try{
-          observer.next('Sang');
-          setInterval(()=>{
-            observer.next('Hoa');
-          },2000);
-          setInterval(()=>{
-            observer.next('Uyên');
-          },5000);
+  //public students: Observable<string> | undefined = of(this.listStudents);
 
-        }
-        catch(e){
-          observer.error(e);
+  public students$: Observable<any> | undefined = of(this.listStudents);
 
-        }
+  constructor() {}
+
+  ngOnInit(): void {
+    this.agents = new Observable(function (observer) {
+      try {
+        observer.next('Sang');
+        setInterval(() => {
+          observer.next('Hoa');
+        }, 2000);
+        setInterval(() => {
+          observer.next('Uyên');
+        }, 5000);
+      } catch (e) {
+        observer.error(e);
       }
-    );
-
-    this.agents.subscribe(data => {
-      //console.log(data);
-      this.agentName = data;
-
     });
 
+    this.agents.subscribe((data) => {
+      //console.log(data);
+      this.agentName = data;
+    });
+
+    // this.students?.subscribe((data) => {
+    //   console.log(data);
+    // });
+
+    this.students$?.subscribe((data) => {
+      console.log(data);
+    });
+
+    // ep 4
+
+    // of
   }
 }
